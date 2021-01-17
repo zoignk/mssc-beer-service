a0d99ec420e9dbd7592d51adfbd313823461a8d6
zoignk <zoranboshkovic@yahoo.com>
2021-01-17 09:43:19 +0100

    Add jackson config to BeerDto closes #13

--- src/main/java/zoig/springframwork/msscbeerservice/web/model/BeerDto.java
+++ src/main/java/zoig/springframwork/msscbeerservice/web/model/BeerDto.java
@@ -1,5 +1,6 @@
 package zoig.springframwork.msscbeerservice.web.model;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -24,8 +25,10 @@ public class BeerDto {
     private Integer version;
 
     @Null
+    @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ssZ", shape = JsonFormat.Shape.STRING)
     private OffsetDateTime createdDate;
     @Null
+    @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ssZ", shape = JsonFormat.Shape.STRING)
     private OffsetDateTime lastModifiedDate;
     @NotBlank
     private String beerName;
@@ -37,6 +40,7 @@ public class BeerDto {
     @NotNull
     private long upc;
 
+    @JsonFormat(shape = JsonFormat.Shape.STRING)
     @Positive
     @NotNull
     private BigDecimal price;
