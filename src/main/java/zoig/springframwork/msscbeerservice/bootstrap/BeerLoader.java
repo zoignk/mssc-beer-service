3b7fbfdbebcca76b97f3278e6849a34f58215b2e
zoignk <zoranboshkovic@yahoo.com>
2021-01-18 09:24:23 +0100

    load data from data.sql, closes #17

--- src/main/java/zoig/springframwork/msscbeerservice/bootstrap/BeerLoader.java
+++ src/main/java/zoig/springframwork/msscbeerservice/bootstrap/BeerLoader.java
@@ -6,6 +6,7 @@ import zoig.springframwork.msscbeerservice.domain.Beer;
 import zoig.springframwork.msscbeerservice.repositories.BeerRepository;
 
 import java.math.BigDecimal;
+import java.util.UUID;
 
 @Component
 public class BeerLoader implements CommandLineRunner {
@@ -13,6 +14,9 @@ public class BeerLoader implements CommandLineRunner {
     public static final String BEER_1_UPC = "0631234200036";
     public static final String BEER_2_UPC = "0631234300019";
     public static final String BEER_3_UPC = "0083783375213";
+    public static final UUID BEER_1_UUID = UUID.fromString("0a818933-087d-47f2-ad83-2f986ed087eb");
+    public static final UUID BEER_2_UUID = UUID.fromString("a712d914-61ea-4623-8bd0-32c0f6545bfd");
+    public static final UUID BEER_3_UUID = UUID.fromString("026cc3c8-3a0c-4083-a05b-e908048c1b08");
 
     private final BeerRepository beerRepository;
 
@@ -22,39 +26,7 @@ public class BeerLoader implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
-        loadBeerObjects();
-    }
-
-    private void loadBeerObjects() {
-        if (beerRepository.count() == 0) {
-            beerRepository.save(Beer.builder()
-                    .beerName("Mango Bobs")
-                    .beerStyle("IPA")
-                    .quantityToBrew(200)
-                    .minOnHand(12)
-                    .upc(BEER_1_UPC)
-                    .price(new BigDecimal("12.95"))
-                    .build());
-
-            beerRepository.save(Beer.builder()
-                    .beerName("Galaxy Cat")
-                    .beerStyle("PALE_ALE")
-                    .quantityToBrew(200)
-                    .minOnHand(12)
-                    .upc(BEER_2_UPC)
-                    .price(new BigDecimal("11.95"))
-                    .build());
-
-            beerRepository.save(Beer.builder()
-                    .beerName("No Hammers On The Bar")
-                    .beerStyle("PALE_ALE")
-                    .quantityToBrew(200)
-                    .minOnHand(12)
-                    .upc(BEER_3_UPC)
-                    .price(new BigDecimal("11.95"))
-                    .build());
-        }
-
-        System.out.println("Loaded Beers: " + beerRepository.count());
+        // using data.sql file now
+        // loadBeerObjects();
     }
 }
